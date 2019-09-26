e12249564f39360316c4bc73f1161a83d84f274b
Michael Palma <mpalma@toronto.ca>
2019-09-26 11:38:51 -0400

    Added output variables
    Added output variables for custom select components

--- force-app/main/default/aura/InitiativeProgramSelect/InitiativeProgramSelectController.js
+++ force-app/main/default/aura/InitiativeProgramSelect/InitiativeProgramSelectController.js
@@ -10,14 +10,20 @@
 
     },
 
-    onChange: function (cmp, evt, helper) {
+    onChangeInitiative: function (cmp, evt, helper) {
         var parentId = cmp.find('initiativeSelect').get('v.value');
 
+        cmp.set('v.initiativeIdSelected', parentId);
         helper.apex(cmp, "getAllActiveProgramsByParentId", {parentId : parentId})
         .then(function (result) {
             cmp.set('v.programs', result);
         }).catch(function (error) {
             //do something about the error
         });
+    },
+
+    onChangeProgram: function (cmp, evt, helper) {
+        var programId = cmp.find('programSelect').get('v.value');
+        cmp.set('v.programIdSelected', programId);
     }
 })
--- force-app/main/default/aura/NPSPAuraFlow/NPSPAuraFlowController.js
+++ force-app/main/default/aura/NPSPAuraFlow/NPSPAuraFlowController.js
@@ -20,10 +20,11 @@
         if(event.getParam("status") === "FINISHED") {
            var outputVariables = event.getParam("outputVariables");
            var outputVar;
-           for(var i = 0; i < outputVariables.length; i++) {
-              outputVar = outputVariables[i];
-              console.log(outputVar);
-           }
+           console.log(outputVariables);
+         //   for(var i = 0; i < outputVariables.length; i++) {
+         //      outputVar = outputVariables[i];
+         //      console.log(outputVar);
+         //   }
         }
      }
 })
