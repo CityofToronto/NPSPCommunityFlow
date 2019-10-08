bbcfa4904aefee97d54627f48c2efab0eb5d52c7
Michael Palma <mpalma@toronto.ca>
2019-10-08 14:47:27 -0400

    helper functions added to pull custommetadata
    -pulling application settings from custommetadata
    -added encryption functions

--- force-app/main/default/aura/NPSPAuraFlow/NPSPAuraFlowController.js
+++ force-app/main/default/aura/NPSPAuraFlow/NPSPAuraFlowController.js
@@ -10,24 +10,40 @@
             }
         ];
 
-        // Find the component whose aura:id is "flowData"
-        var flow = component.find("flowData");
-        // In that component, start your flow. Reference the flow's Unique Name.
-        flow.startFlow("DonateTO", flowInputVariables);
+        helper.soql(component, "SELECT Label, AES_Key_256__c, Moneris_HPP_Key__c, Moneris_Store_Id__c, Moneris_URL__c, Flow_Name__c FROM DMS_Settings__mdt")
+        .then(function (settings) {
+            settings.forEach(function(item, index) {
+                helper.settingsObj[item.Label] = item;
+            });
+            // Find the component whose aura:id is "flowData"
+            var flow = component.find("flowData");
+            // In that component, start your flow. Reference the flow's Unique Name.
+            flow.startFlow(helper.settingsObj.DEV.Flow_Name__c, flowInputVariables);
+
+            component.set('v.dmsSettings', helper.settingsObj);
+        })
     },
         
-    handleStatusChange : function (component, event) {
-        event.preventDefault();
+    handleStatusChange : function (component, event, helper) {
         if(event.getParam("status") === "FINISHED") {
             var outputVariables = event.getParam("outputVariables");
-            var outputVar;
-            console.log(outputVariables);
-         //   for(var i = 0; i < outputVariables.length; i++) {
-         //      outputVar = outputVariables[i];
-         //      console.log(outputVar);
-         //   }
+            var formResults = {};
 
-            component.find("paymentForm").getElement().submit();
+            outputVariables.forEach(function(item, index) {
+                formResults[item.name] = item;
+            });
+
+            console.log(formResults);
+            component.find("paymentAmount").set("v.value", formResults.Amount_Value.value);
+
+            helper.apex(component, "encryptData", { value : 'test this string'})
+            .then(function (result) {
+                console.log(result);
+            }).catch(function (error) {
+                //do something about the error
+            });
+
+            //component.find("paymentForm").getElement().submit();
         }
      }
 })
