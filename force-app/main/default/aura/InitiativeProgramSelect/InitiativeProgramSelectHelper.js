eefc93eee42b5db9ff4287dd2f0d57737bb3583e
Michael Palma <quikbeam@gmail.com>
2019-10-10 20:08:23 -0400

    added new class, clean up

--- force-app/main/default/aura/InitiativeProgramSelect/InitiativeProgramSelectHelper.js
+++ force-app/main/default/aura/InitiativeProgramSelect/InitiativeProgramSelectHelper.js
@@ -25,33 +25,5 @@
             });
             $A.enqueueAction(action);
         });
-    },
-
-    soql: function (cmp, query) {
-        return new Promise(function (resolve, reject) {
-            var action = cmp.get("c.executeSoql");
-            action.setParams({soql: query});
-            action.setCallback(this, function(response) {
-                var state = response.getState();
-                if (state === "SUCCESS") {
-                    resolve(response.getReturnValue());
-                }
-                else if (state === "INCOMPLETE") {
-                    // do something
-                }
-                else if (state === "ERROR") {
-                    var errors = response.getError();
-                    if (errors && errors[0] && errors[0].message) {
-                        console.log("Error message: " +
-                                    errors[0].message);
-                        reject(errors[0].message);
-                    } else {
-                        console.log("Unknown error");
-                        reject("Unknown error");
-                    }
-                }
-            });
-            $A.enqueueAction(action);
-        });
     }
 })
